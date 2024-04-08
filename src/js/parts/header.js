9e7acc18060721d11db760b75d3ecf5ba007b190
Yevhenii2022 <3starosta3@gmail.com>
2024-04-08 22:12:12 +0300

    some fix

--- src/js/parts/header.js
+++ src/js/parts/header.js
@@ -1,310 +1,127 @@
-document.addEventListener(
-	'click',
-	function (event) {
-		const headerSearch =
-			document.querySelector(
-				'.header__search',
-			);
-		const isClickInside =
-			headerSearch.contains(
-				event.target,
-			);
+document.addEventListener('click', function (event) {
+	const headerSearch = document.querySelector('.header__search');
+	const isClickInside = headerSearch.contains(event.target);
 
-		if (
-			!isClickInside
-		) {
-			headerSearch.classList.remove(
-				'active',
-			);
+	if (!isClickInside) {
+		headerSearch.classList.remove('active');
+	}
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+	const header = document.querySelector('.header');
+	const body = document.body;
+
+	const headerLogo = document.querySelector('.header__logo');
+	const headerNav = document.querySelector('.header__nav');
+	const customSelect = document.querySelector('.custom-select');
+	const headerBtn = document.querySelector('.header--js');
+	const headerBurger = document.querySelector('.header__burger');
+	const headerSearch = document.querySelector('.header__search');
+	const headerBasket = document.querySelector('.header__basket');
+
+	let scrollPosition = window.pageYOffset;
+
+	if (scrollPosition > 80) {
+		header.style.backgroundColor = '#FFFFFF';
+		header.classList.add('header--hidden');
+	} else {
+		if (header.classList.contains('header--hidden')) {
+			header.classList.remove('header--hidden');
+			header.style.backgroundColor = '';
 		}
-	},
-);
+	}
 
-document.addEventListener(
-	'DOMContentLoaded',
-	function () {
-		const header =
-			document.querySelector(
-				'.header',
-			);
-		const body =
-			document.body;
+	let scrollPrev = 0;
 
-		const headerLogo =
-			document.querySelector(
-				'.header__logo',
-			);
-		const headerNav =
-			document.querySelector(
-				'.header__nav',
-			);
-		const customSelect =
-			document.querySelector(
-				'.custom-select',
-			);
-		const headerBtn =
-			document.querySelector(
-				'.header--js',
-			);
-		const headerBurger =
-			document.querySelector(
-				'.header__burger',
-			);
-		const headerSearch =
-			document.querySelector(
-				'.header__search',
-			);
-		const headerBasket =
-			document.querySelector(
-				'.header__basket',
-			);
+	window.addEventListener('scroll', function () {
+		var scrolled = window.pageYOffset || document.documentElement.scrollTop;
 
-		let scrollPosition =
-			window.pageYOffset;
-
-		if (
-			scrollPosition >
-			80
-		) {
-			header.style.backgroundColor =
-				'#FFFFFF';
-			header.classList.add(
-				'header--hidden',
-			);
+		if (scrolled > 80 && scrolled > scrollPrev) {
+			header.classList.add('header--hidden');
 		} else {
-			if (
-				header.classList.contains(
-					'header--hidden',
-				)
-			) {
-				header.classList.remove(
-					'header--hidden',
-				);
-				header.style.backgroundColor =
-					'';
+			if (header.classList.contains('header--hidden')) {
+				header.classList.remove('header--hidden');
+			}
+		}
+		scrollPrev = scrolled;
+
+		if (body.classList.contains('home')) {
+			if (scrolled > 80) {
+				headerLogo.classList.remove('header__logo--filter');
+				headerNav.classList.remove('header--white');
+				customSelect.classList.remove('header--white');
+				headerBtn.classList.remove('btn--white');
+				headerBurger.classList.remove('burger--white');
+				headerBasket.classList.remove('header--stroke');
+				headerSearch.classList.remove('header--white', 'header--fill');
+			} else {
+				headerLogo.classList.add('header__logo--filter');
+				headerNav.classList.add('header--white');
+				customSelect.classList.add('header--white');
+				headerBtn.classList.add('btn--white');
+				headerBurger.classList.add('burger--white');
+				headerBasket.classList.add('header--stroke');
+				headerSearch.classList.add('header--white', 'header--fill');
 			}
 		}
 
-		let scrollPrev = 0;
-
-		window.addEventListener(
-			'scroll',
-			function () {
-				var scrolled =
-					window.pageYOffset ||
-					document
-						.documentElement
-						.scrollTop;
-
-				if (
-					scrolled >
-						80 &&
-					scrolled >
-						scrollPrev
-				) {
-					header.classList.add(
-						'header--hidden',
-					);
-				} else {
-					if (
-						header.classList.contains(
-							'header--hidden',
-						)
-					) {
-						header.classList.remove(
-							'header--hidden',
-						);
-					}
-				}
-				scrollPrev =
-					scrolled;
-
-				if (
-					body.classList.contains(
-						'home',
-					)
-				) {
-					if (
-						scrolled >
-						80
-					) {
-						headerLogo.classList.remove(
-							'header__logo--filter',
-						);
-						headerNav.classList.remove(
-							'header--white',
-						);
-						customSelect.classList.remove(
-							'header--white',
-						);
-						headerBtn.classList.remove(
-							'btn--white',
-						);
-						headerBurger.classList.remove(
-							'burger--white',
-						);
-						headerBasket.classList.remove(
-							'header--stroke',
-						);
-						headerSearch.classList.remove(
-							'header--white',
-							'header--fill',
-						);
-					} else {
-						headerLogo.classList.add(
-							'header__logo--filter',
-						);
-						headerNav.classList.add(
-							'header--white',
-						);
-						customSelect.classList.add(
-							'header--white',
-						);
-						headerBtn.classList.add(
-							'btn--white',
-						);
-						headerBurger.classList.add(
-							'burger--white',
-						);
-						headerBasket.classList.add(
-							'header--stroke',
-						);
-						headerSearch.classList.add(
-							'header--white',
-							'header--fill',
-						);
-					}
-				}
-
-				if (
-					scrolled >
-					80
-				) {
-					header.style.backgroundColor =
-						'#FFFFFF';
-				} else {
-					header.style.backgroundColor =
-						'';
-				}
-			},
-		);
-
-		//зміна кольору хедера по кліку на кнопку бургер
-		headerBurger.addEventListener(
-			'click',
-			function () {
-				setTimeout(
-					function () {
-						if (
-							headerBurger.classList.contains(
-								'active',
-							)
-						) {
-							header.style.backgroundColor =
-								'#eaf2f5';
-						} else {
-							if (
-								window.pageYOffset >
-								100
-							) {
-								header.style.backgroundColor =
-									'#FFFFFF';
-							} else {
-								header.style.backgroundColor =
-									'';
-							}
-						}
-					},
-					50,
-				);
-			},
-		);
-		//зміна кольору хедера по кліку на кнопку бургер
-
-		const searchInput =
-			document.querySelector(
-				'.header__input',
-			);
-
-		if (
-			headerSearch
-		) {
-			headerSearch.addEventListener(
-				'click',
-				function (
-					event,
-				) {
-					event.stopPropagation();
-
-					headerSearch.classList.toggle(
-						'active',
-					);
-
-					if (
-						headerSearch.classList.contains(
-							'active',
-						)
-					) {
-						searchInput.focus();
-					}
-				},
-			);
+		if (scrolled > 80) {
+			header.style.backgroundColor = '#FFFFFF';
+		} else {
+			header.style.backgroundColor = '';
 		}
+	});
 
-		if (
-			searchInput
-		) {
-			searchInput.addEventListener(
-				'click',
-				function (
-					event,
-				) {
-					event.stopPropagation();
-				},
-			);
-		}
-
-		document.addEventListener(
-			'click',
-			function (
-				event,
-			) {
-				if (
-					!searchInput.contains(
-						event.target,
-					)
-				) {
-					headerSearch.classList.remove(
-						'active',
-					);
+	//зміна кольору хедера по кліку на кнопку бургер
+	headerBurger.addEventListener('click', function () {
+		setTimeout(function () {
+			if (headerBurger.classList.contains('active')) {
+				header.style.backgroundColor = '#eaf2f5';
+			} else {
+				if (window.pageYOffset > 100) {
+					header.style.backgroundColor = '#FFFFFF';
+				} else {
+					header.style.backgroundColor = '';
 				}
-			},
-		);
+			}
+		}, 50);
+	});
+	//зміна кольору хедера по кліку на кнопку бургер
 
-		//burger menu
-		const menu =
-			document.querySelector(
-				'.header__overlay',
-			);
-		const menuButton =
-			document.querySelector(
-				'.burger',
-			);
+	const searchInput = document.querySelector('.header__input');
 
-		menuButton.addEventListener(
-			'click',
-			function () {
-				this.classList.toggle(
-					'active',
-				);
-				menu.classList.toggle(
-					'open',
-				);
-				document.body.classList.toggle(
-					'burger-open',
-				);
-			},
-		);
-		//burger menu
-	},
-);
+	if (headerSearch) {
+		headerSearch.addEventListener('click', function (event) {
+			event.stopPropagation();
+
+			headerSearch.classList.toggle('active');
+
+			if (headerSearch.classList.contains('active')) {
+				searchInput.focus();
+			}
+		});
+	}
+
+	if (searchInput) {
+		searchInput.addEventListener('click', function (event) {
+			event.stopPropagation();
+		});
+	}
+
+	document.addEventListener('click', function (event) {
+		if (!searchInput.contains(event.target)) {
+			headerSearch.classList.remove('active');
+		}
+	});
+
+	//burger menu
+	const menu = document.querySelector('.header__overlay');
+	const menuButton = document.querySelector('.burger');
+
+	menuButton.addEventListener('click', function () {
+		this.classList.toggle('active');
+		menu.classList.toggle('open');
+		document.body.classList.toggle('burger-open');
+	});
+	//burger menu
+});
--- template-parts/form.php
+++ template-parts/form.php
@@ -2,30 +2,41 @@
 
 <!-- <div class="form__wrapper">
    <div class="form__input">
-     <label>[text* name-1 autocomplete:off placeholder "Имя"]
+     <label>[text* name-1 autocomplete:off placeholder "{Имя}"]
       <svg class="form__icon" viewBox="0 0 19 19">
        <path stroke="#FF4040" stroke-linecap="round" d="m5.243 13.243 8.485-8.486M5.243 4.757l8.485 8.486"/>
       </svg>
      </label>
    </div>
    <div class="form__input">
-     <label>[tel* phone autocomplete:off placeholder "Номер телефона"]
+     <label>[tel* phone autocomplete:off placeholder "{Номер телефона}"]
       <svg class="form__icon" viewBox="0 0 19 19">
        <path stroke="#FF4040" stroke-linecap="round" d="m5.243 13.243 8.485-8.486M5.243 4.757l8.485 8.486"/>
       </svg>
      </label>
    </div>
    <div class="form__input">
-     <label>[email* email-770 autocomplete:off placeholder "Почта"]
+     <label>[email* email-770 autocomplete:off placeholder "{Почта}"]
       <svg class="form__icon" viewBox="0 0 19 19">
        <path stroke="#FF4040" stroke-linecap="round" d="m5.243 13.243 8.485-8.486M5.243 4.757l8.485 8.486"/>
      </svg>
 </label>
    </div>
    <div class="form__input">
-     <label>[textarea task placeholder "Комментарий"]</label>
+     <label>[textarea task placeholder "{Комментарий}"]</label>
    </div>
   <div>
-    [submit class:form__button "Заказать консультацию"]	   
+    <label><div class="btn">
+            <div class="btn__text">
+                <span>{Заказать консультацию} </span>
+                <span>{Заказать консультацию} </span>
+            </div>
+
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 11 12" fill="none">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M5.849 9.637 9.485 6m0 0L5.85 2.363M9.485 6H1" />
+            </svg>
+        </div>
+        [submit class:form__button "{Заказать консультацию}"]	
+ </label> 
   </div>
 </div> -->
